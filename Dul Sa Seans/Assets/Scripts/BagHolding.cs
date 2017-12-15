1a505182f445f4923c10383db67f4f7a8864c47b
siouxmcd <claeomcdermott@u.boisestate.edu>
2017-12-14 23:28:45 -0700

    WEHAVESNEAKYSNEAKYSTEALTH!
    and also sound

---
+++ Dul Sa Seans/Assets/Scripts/BagHolding.cs
@@ -0,0 +1,37 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class BagHolding : MonoBehaviour {
+
+    public GameObject bag;
+    public GameUI gameUI;
+    public AudioClip coinDrop;
+    private AudioSource bagSoundEffects;
+
+	// Use this for initialization
+	void Start () {
+        bagSoundEffects = GetComponent<AudioSource>();
+	}
+	
+	// Update is called once per frame
+	void Update () {
+		
+	}
+
+    private void OnTriggerEnter(Collider other)
+    {
+        if (other.tag == "Coin")
+        {
+            gameUI.increaseScore(15);
+            Destroy(other.gameObject);
+            bagSoundEffects.PlayOneShot(coinDrop);
+        }
+        if (other.tag == "Goblet")
+        {
+            gameUI.increaseScore(150);
+            Destroy(other.gameObject);
+            bagSoundEffects.PlayOneShot(coinDrop);
+        }
+    }
+}
